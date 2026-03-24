26b6d535023f95947ba4416ce8b8c9b905dafa32
darlanzerodev <darliankeira229@gmail.com>
2026-03-24 16:33:35 -0300

    feat: adicionar interceptor de autenticação para gerenciar tokens

---
+++ age-signaturehub-web/src/app/core/interceptors/auth.interceptor.ts
@@ -0,0 +1,7 @@
+import { inject } from "@angular/core";
+import { AuthService } from "../services/auth.service";
+
+export const authInterceptor = (req, next) => {
+  const authService = inject(AuthService);
+  const authToken = authService.getToken();
+}
