7752e3baef68d20b98bfa966907a58480874003e
darlanzerodev <darliankeira229@gmail.com>
2026-04-09 16:02:36 -0300

    feat: implementar componente de dashboard com layout e funcionalidades iniciais

--- age-signaturehub-web/src/app/features/internal/dashboard/dashboard.component.ts
+++ age-signaturehub-web/src/app/features/internal/dashboard/dashboard.component.ts
@@ -1,9 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatAnchor } from "@angular/material/button";
+import { MatIcon } from "@angular/material/icon";
+import { MatCard } from "@angular/material/card";
+
+interface StatCard {
+  title: string;
+  value: string | number;
+  icon: string;
+  color: string;
+  trend?: {
+    value: string;
+    isPositive: boolean;
+  }
+}
+
+interface RecentDocument {
+  id: string;
+  title: string;
+  status: 'pending' | 'signed' | 'rejected' | 'expired';
+  createdAt: Date;
+  signers: number;
+  signedcount: number;
+}
+
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
-  templateUrl: './dashboard.html',
-  styleUrl: './dashboard.scss',
+  imports: [MatAnchor, MatIcon, MatCard],
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss'],
 })
-export class Dashboard {}
+export class DashboardComponent implements OnInit {
+
+  userName = '';
+
+  stats: StatCard[] = [
+    {
+      
+    }
+  ]
+}
