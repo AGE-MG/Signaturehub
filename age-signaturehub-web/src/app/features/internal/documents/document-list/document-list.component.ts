43d1567ee8db9196073ff72109cae5475da21d08
DarlanZeroDev <darliankeira229@gmail.com>
2026-05-05 15:48:53 -0300

    feat: adicionar filtros de busca e status na lista de documentos

--- age-signaturehub-web/src/app/features/internal/documents/document-list/document-list.component.ts
+++ age-signaturehub-web/src/app/features/internal/documents/document-list/document-list.component.ts
@@ -8,10 +8,19 @@ import { DocumentStatus } from '../../../../core/models/dasboard.model';
 import { Router } from '@angular/router';
 import { DocumentService } from '../../../../core/services/document.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatAnchor } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatFormField, MatLabel } from "@angular/material/form-field";
+import { MatInput } from "@angular/material/input";
+import { MatSelect, MatOption } from "@angular/material/select";
+import { FormsModule } from "@angular/forms";
+import { MatChipSet, MatChip } from "@angular/material/chips";
+import { MatCard } from "@angular/material/card";
+import { MatProgressSpinner } from "@angular/material/progress-spinner";
 
 @Component({
   selector: 'app-document-list',
-  imports: [MatPaginatorModule, MatSortModule],
+  imports: [MatPaginatorModule, MatSortModule, MatAnchor, MatIconModule, MatFormField, MatLabel, MatInput, MatSelect, FormsModule, MatOption, MatChipSet, MatChip, MatCard, MatProgressSpinner],
   templateUrl: './document-list.component.html',
   styleUrls: ['./document-list.component.scss'],
 })
