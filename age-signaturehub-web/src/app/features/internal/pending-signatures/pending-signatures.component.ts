4ef3badff3ad0e5bd060bc7c2dea489fb53b7d7c
darlanzerodev <darliankeira229@gmail.com>
2026-06-03 14:18:58 -0300

    feat: adicionar lógica de carregamento e exibição de assinaturas pendentes no componente de assinaturas

--- age-signaturehub-web/src/app/features/internal/pending-signatures/pending-signatures.component.ts
+++ age-signaturehub-web/src/app/features/internal/pending-signatures/pending-signatures.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { SignatureStatus, SignatureStatusColor, SignatureStatusLabel, SignatureTypeIcon, SignatureTypeLabel, SignerDto, SignerRoleLabel } from '../../../core/models/signer.model';
+import { AuthService } from '../../../core/services/auth.service';
+import { SignerService } from '../../../core/services/signer.service';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pending-signatures',
@@ -15,6 +21,42 @@ import { MatIconModule } from '@angular/material/icon';
   templateUrl: './pending-signatures.component.html',
   styleUrl: './pending-signatures.component.scss',
 })
-export class PendingSignaturesComponent {
+export class PendingSignaturesComponent implements OnInit {
 
+  signers: SignerDto[] = [];
+  loading = false;
+
+  readonly SignatureStatus = SignatureStatus;
+  readonly SignatureStatusLabel = SignatureStatusLabel;
+  readonly SignatureStatusColor= SignatureStatusColor;
+  readonly SignatureTypeLabel = SignatureTypeLabel;
+  readonly SignatureTypeIcon = SignatureTypeIcon;
+  readonly SignerRoleLabel = SignerRoleLabel;
+
+  constructor(
+    private authService: AuthService,
+    private signerService: SignerService,
+    private dialog: MatDialog,
+    private snackbar: MatSnackBar,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.load()
+  }
+
+  load(): void {
+    const user = this.authService.getUserValue();
+    if (!user?.email) return;
+    this.loading = true;
+    this.signerService.getPendingByEmail(user.email).subscribe({
+      next: (list) => {
+        this;getPend
+      },
+      error: (err) => {
+        this.loading = false;
+        this.snackbar.open(err?.error?.message || 'Failed to load pending signatures', 'Close', { duration: 5000 });
+      }
+    })
+  }
 }
