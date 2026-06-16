85ff9b545c2b2fb087e2f1c41874f841482fda86
darlanzerodev <darliankeira229@gmail.com>
2026-06-16 15:56:31 -0300

    feat: adicionar validação de senha e formulários ao componente de configurações

--- age-signaturehub-web/src/app/features/internal/settings/settings.component.ts
+++ age-signaturehub-web/src/app/features/internal/settings/settings.component.ts
@@ -1,4 +1,13 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { UserDto } from '../../../core/models/signer.model';
+import { MatTableDataSource } from '@angular/material/table';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+
+function passwordMatchValidator(control: AbstractControl): ValidationErrors | null {
+  const newPass = control.get('newPassword')?.value;
+  const confirmPassword = control.get('confirmPassword')?.value;
+  return newPass && confirmPassword && newPass !== confirmPassword ? { mismatch: true } : null;
+}
 
 @Component({
   selector: 'app-settings.component',
@@ -8,6 +17,36 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SettingsComponent implements OnInit {
-  constructor() {}
+  currentUser: UserDto | null = null;
+  loadingProfile = false;
+  savingProfile = false;
+  savingPassword = false;
+
+  usersDataSource = new MatTableDataSource<UserDto>();
+  usersDisplayedColumns = ['avatar', 'nome', 'email', 'função', 'ações'];
+  loadingUsers = false;
+
+  profileForm!: FormGroup
+  passwordForm!: FormGroup
+
+  hideCurrentPassword = true;
+  hideNewPassword = true;
+  hideConfirmPassword = true;
+
+  constructor(
+    private fb: FormBuilder,
+  ) {
+    this.profileForm = this.fb.group({
+    fullName: ['', [Validators.required, Validators.minLength(3)]],
+    department: [''],
+    position: [''],
+    registrationNumber: ['']
+  }),
+    this.passwordForm = this.fb.group({
+    currentPassword: ['', [Validators.required]],
+    newPassword: ['', [Validators.required, Validators.minLength(6)]],
+    confirmPassword: ['', [Validators.required, Validators.minLength(6)]]
+  }, {validators: passwordMatchValidator})
+  }
   ngOnInit(): void {}
 }
